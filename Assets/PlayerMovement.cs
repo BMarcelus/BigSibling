479a23736e4f10c3bd32efdaa1007f2dc64e9b14
BMarcelus <bmdizon@uci.edu>
2021-08-21 12:43:08 -0700

    movement and dialogue test

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 4f;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 targetVelocity = new Vector3(
+            horizontal*speed,
+            vertical*speed,
+            0
+        );
+        rigidbody.velocity = targetVelocity;
+    }
+}
